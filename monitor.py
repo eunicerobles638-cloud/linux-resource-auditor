5fbff976d3b3ee9c759b8c7c0e55f207f1f58d61
Eunice_Robles <eunicerobles638gmail.com>
2026-03-01 19:46:38 +0800

    feat: update monitor logic

--- monitor.py
+++ monitor.py
@@ -1,5 +1,21 @@
 import os
-print("DevOps Monitoring Initialized...")
-storage = os.popen("df -h /data/data/com.termux/files/home").read()
-print(f"Current Status:\n{storage}")
 
+print("--- SRE SMART MONITOR ACTIVE ---")
+
+storage_data = os.popen("df -h $HOME").read()
+print(f"Current Status\n{storage data}")
+
+storage_data.split('\n'):
+    if '/data/data/com.termux'
+        colums = line.split()
+        usage_pct = colums[4].replace('%', '')
+        usage_int = int(usage_pct)
+
+        print(f"\n[ANALYSIS]: Current Usage is at {usage_int}%")
+
+        if usage_int >= 90:
+            print("❌ CRITICAL: Storage is almost full! Immediate cleanup required.")
+        elif usage_int >= 75:
+            print("⚠️ WARNING: Storage usage is high. Monitor closely.")
+        else:
+            print("✅ HEALTHY: Storage levels are within normal limits.")
