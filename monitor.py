eb59f40846b9b06a73976482f6c00d75d86a7fa8
Eunice_Robles <eunicerobles638gmail.com>
2026-03-01 20:00:26 +0800

    feat: update monitor logic

--- monitor.py
+++ monitor.py
@@ -3,12 +3,12 @@ import os
 print("--- SRE SMART MONITOR ACTIVE ---")
 
 storage_data = os.popen("df -h $HOME").read()
-print(f"Current Status\n{storage data}")
+print(f"Current Status:\n{storage_data}")
 
-storage_data.split('\n'):
-    if '/data/data/com.termux'
-        colums = line.split()
-        usage_pct = colums[4].replace('%', '')
+for line in storage_data.split('\n'):
+    if '/data/data/com.termux' in line:
+        columns = line.split()
+        usage_pct = columns[4].replace('%', '')
         usage_int = int(usage_pct)
 
         print(f"\n[ANALYSIS]: Current Usage is at {usage_int}%")
@@ -19,3 +19,4 @@ storage_data.split('\n'):
             print("⚠️ WARNING: Storage usage is high. Monitor closely.")
         else:
             print("✅ HEALTHY: Storage levels are within normal limits.")
+
