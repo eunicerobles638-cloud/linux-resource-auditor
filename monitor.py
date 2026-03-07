698a6cd95a02b600b66e509ca0c4677695513500
Eunice_Robles <eunicerobles638gmail.com>
2026-03-08 01:44:37 +0800

    feat: final monito with alert

--- monitor.py
+++ monitor.py
@@ -5,7 +5,6 @@ import os
 def check_system_health():
     total, used, free = shutil.disk_usage("/")
     disk_percent = (used / total) * 100
-    
     ram_percent = psutil.virtual_memory().percent
     
     print(f"--- SYSTEM REPORT ---")
@@ -14,6 +13,7 @@ def check_system_health():
     
     if disk_percent > 90 or ram_percent > 90:
         print("ALERT: CRITICAL STATUS!")
+        os.system('termux-notification --title "SRE ALERT" --content "Critical Resource Usage! Disk: {:.2f}% RAM: {:.2f}%" --priority high'.format(disk_percent, ram_percent))
     else:
         print("Status: HEALTHY ✅")
 
